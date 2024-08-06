414608e13200fad844d9e39ff5ea84468131d91f
Nipuna Gamage <147525162+Nipuna-Gamage@users.noreply.github.com>
2024-08-06 09:10:11 +0530

    create other page and routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('devicemanagement/home');
 });
+
+Route::get('register', function () {
+    return view('devicemanagement/register');
+});
+
+Route::get('update', function () {
+    return view('devicemanagement/update');
+});
+
+Route::get('checkdevice', function () {
+    return view('devicemanagement/checkdevice');
+});
